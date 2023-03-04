65f66fcf48d0ce514e50d3f24d33d214a6ade47d
Nithin Pradeep <nithinpradeep@Nithins-MacBook-Air.local>
2023-03-04 16:32:24 +0530

    Added Prettier

--- src/components/layouts/Layout/Layout.jsx
+++ src/components/layouts/Layout/Layout.jsx
@@ -1,17 +1,13 @@
 import styles from './Layout.module.scss';
 import PropTypes from 'prop-types';
 
-function Layout(props){
-    return(
-        <div className={styles['layout']}>
-            {props.children}
-        </div>
-    )
+function Layout(props) {
+	return <div className={styles['layout']}>{props.children}</div>;
 }
 
 Layout.propTypes = {
-    children: PropTypes.object.isRequired,
-    data: PropTypes.object
-}
+	children: PropTypes.object.isRequired,
+	data: PropTypes.object,
+};
 
 export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Portfolio Nithin</title>
-        <meta name="description" content="A Portfolio Website Created by Nithin Pradeep" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>Portfolio</div>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Portfolio Nithin</title>
+				<meta
+					name="description"
+					content="A Portfolio Website Created by Nithin Pradeep"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div>Portfolio</div>
+		</>
+	);
 }
