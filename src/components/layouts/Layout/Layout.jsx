a5013dddba66b30ae2363df7a424b76a35a4c817
Nithin Pradeep <nithinpradeep@Nithins-MacBook-Air.local>
2023-03-12 00:13:24 +0530

    Added Navbar component

--- src/components/layouts/Layout/Layout.jsx
+++ src/components/layouts/Layout/Layout.jsx
@@ -1,8 +1,14 @@
 import styles from './Layout.module.scss';
 import PropTypes from 'prop-types';
+import Navbar from '../Navbar/Navbar';
 
 function Layout(props) {
-	return <div className={styles['layout']}>{props.children}</div>;
+	return (
+		<div className={styles['layout']}>
+			{props.children}
+			<Navbar />
+		</div>
+	);
 }
 
 Layout.propTypes = {
