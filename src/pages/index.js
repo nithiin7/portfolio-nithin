9c164a412b48266493d2f157145b4944e20b37e7
Nithin Pradeep <nithinpradeep@Nithins-MacBook-Air.local>
2023-03-12 21:21:32 +0530

    Added contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,11 +20,29 @@ import ServiceCard from 'components/pages/ServiceCard';
 import PortfolioCard from 'components/pages/PortfolioCard';
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { useRef } from 'react';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { MdOutlineMail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+import emailjs from 'emailjs-com';
 
 export default function Home() {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm(
+			'YOUR_SERVICE_ID',
+			'YOUR_TEMPLATE_ID',
+			form.current,
+			'YOUR_PUBLIC_KEY'
+		);
+
+		e.target.reset();
+	};
 	const data = [
 		{
 			id: 1,
@@ -419,6 +437,93 @@ export default function Home() {
 							);
 						})}
 					</Swiper>
+				</div>
+			</section>
+			<section id="contact">
+				<h5 data-aos="fade-up" data-aos-duration="1000">
+					Get In Touch
+				</h5>
+				<h2 data-aos="fade-up" data-aos-duration="1100">
+					Contact Me
+				</h2>
+
+				<div
+					data-aos="fade-up"
+					data-aos-duration="1200"
+					className="container contact__container"
+				>
+					<div
+						data-aos="fade-up"
+						data-aos-duration="1300"
+						className="contact__options"
+					>
+						<article
+							data-aos="fade-up"
+							data-aos-duration="1400"
+							className="contact__option"
+						>
+							<MdOutlineMail className="contact__option-icon" />
+							<h4>Email</h4>
+							<h5>nithinp150@gmail.com</h5>
+							<a href="mailto:nithinp150@gmail.com">Send a Message</a>
+						</article>
+						<article
+							data-aos="fade-up"
+							data-aos-duration="1500"
+							className="contact__option"
+						>
+							<BsWhatsapp className="contact__option-icon" />
+							<h4>WhatsApp</h4>
+							<h5>+91-9645018007</h5>
+							<a href="https://api.whatsapp.com/send?phone=+919645018007">
+								Send a Message
+							</a>
+						</article>
+						<article
+							data-aos="fade-up"
+							data-aos-duration="1600"
+							className="contact__option"
+						>
+							<RiMessengerLine className="contact__option-icon" />
+							<h4>Messenger</h4>
+							<h5>Nithin Pradeep</h5>
+							<a href="https://facebook.com">Send a Message</a>
+						</article>
+					</div>
+					<form ref={form} onSubmit={sendEmail}>
+						<input
+							data-aos="fade-up"
+							data-aos-duration="1200"
+							type="text"
+							name="name"
+							placeholder="Your Full Name"
+							required
+						/>
+						<input
+							data-aos="fade-up"
+							data-aos-duration="1300"
+							type="email"
+							name="email"
+							placeholder="Your Email"
+							required
+						/>
+						<textarea
+							data-aos="fade-up"
+							data-aos-duration="1400"
+							name="message"
+							rows="7"
+							placeholder="Your Message"
+							required
+						></textarea>
+						<button
+							data-aos="fade-up"
+							data-aos-duration="1500"
+							type="submit"
+							className="btn btn-primary"
+						>
+							Send Message
+						</button>
+					</form>
 				</div>
 			</section>
 		</>
