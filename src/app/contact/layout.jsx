685b6a0ac55e534699b4378f58eea9a59d99db17
Nithin <nithinp150@gmail.com>
2024-10-02 21:41:00 +0530

    Bug fixes

--- src/app/contact/layout.jsx
+++ src/app/contact/layout.jsx
@@ -1,18 +1,12 @@
-import loadData from 'helpers/contentful';
 import PropTypes from 'prop-types';
 
-import 'styles/globals.scss';
-
 export async function generateMetadata() {
-	const props = await loadData('home');
-	const path = props?.data.pageCollection.items[0];
-
 	return {
 		title: 'Contact | Nithin',
 		description: "Let's connect and bring about interesting opportunities",
 		openGraph: {
 			title: 'Nithin Pradeep - Aspiring Developer',
-			description: path.description,
+			description: "Let's connect and bring about interesting opportunities",
 		},
 	};
 }
