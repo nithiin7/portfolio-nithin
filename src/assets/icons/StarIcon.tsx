45e3c48c67be0e43b48512af18e19aea65c91db4
Nithin <nithinp150@gmail.com>
2025-07-19 12:58:18 +0530

    feat: update and added new shapes for services

---
+++ src/assets/icons/StarIcon.tsx
@@ -0,0 +1,21 @@
+import type { FC } from 'react';
+
+const StarIcon: FC = () => (
+	<svg
+		width="80px"
+		height="80px"
+		viewBox="0 0 24 24"
+		fill="transparent"
+		xmlns="http://www.w3.org/2000/svg"
+		aria-hidden="true"
+	>
+		<path
+			d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+			fill="transparent"
+			stroke="rgb(191, 191, 177)"
+			strokeWidth="2"
+		/>
+	</svg>
+);
+
+export default StarIcon;
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as CircleIcon } from './CircleIcon';
+export { default as OvalIcon } from './OvalIcon';
+export { default as StarIcon } from './StarIcon';
