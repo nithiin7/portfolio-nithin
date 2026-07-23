350f618275880417dbc57a0b55cc94f1b7645700
Nithin Pradeep <68104822+nithiin7@users.noreply.github.com>
2026-07-23 13:39:39 +0530

    feat: add reduce motion styles

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { SerwistProvider } from '@serwist/turbopack/react';
 import { ReactLenis } from 'lenis/react';
+import { MotionConfig } from 'motion/react';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 
 import { ThemeProvider } from 'contexts/ThemeContext';
@@ -20,9 +21,11 @@ export default function Provider({
 		>
 			<NuqsAdapter>
 				<ThemeProvider>
-					<main id="main-content" tabIndex={-1}>
-						<ReactLenis root>{children}</ReactLenis>
-					</main>
+					<MotionConfig reducedMotion="user">
+						<main id="main-content" tabIndex={-1}>
+							<ReactLenis root>{children}</ReactLenis>
+						</main>
+					</MotionConfig>
 				</ThemeProvider>
 			</NuqsAdapter>
 		</SerwistProvider>
